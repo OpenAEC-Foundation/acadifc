c3e3ad7b2acf512591ce6acec9519228b45da39f
BjornFidder <bjornfidder@gmail.com>
2026-08-05 15:36:50 +0200

    feat(ifccad): parse conformance manifest

--- src/ifccad/conformance/mod.rs
+++ src/ifccad/conformance/mod.rs
@@ -1,6 +1,15 @@
 //! Language-neutral IFCCAD conformance support.
 
+mod error;
+mod manifest;
+
 use std::path::PathBuf;
+
+pub use error::ConformanceError;
+pub use manifest::{
+    parse_conformance_manifest, ConformanceCase, ConformanceCategory, ConformanceManifest,
+    ConformanceOperation, ConformanceOperationName, ExpectedOutcome,
+};
 
 /// Version of the conformance test collection bundled with this crate.
 pub const BUNDLED_CONFORMANCE_VERSION: &str = "1.0.0";
